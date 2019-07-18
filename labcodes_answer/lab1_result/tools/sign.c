f506bbff50800189b49e80d9b167eff5688d547e
DiDongDongDi <2572931653@qq.com>
2019-07-18 09:30:09 +0800

    添加注释

--- labcodes_answer/lab1_result/tools/sign.c
+++ labcodes_answer/lab1_result/tools/sign.c
@@ -3,24 +3,23 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int
-main(int argc, char *argv[]) {
-    struct stat st;
+int main(int argc, char *argv[]) {//关于main函数参数的问题, 参考https://blog.csdn.net/yhawaii/article/details/7361302
+    struct stat st;//struct stat结构体用来描述一个linux文件系统中的文件属性的结构, 参考https://www.cnblogs.com/CSU-PL/archive/2013/06/06/3120757.html
     if (argc != 3) {
         fprintf(stderr, "Usage: <input filename> <output filename>\n");
         return -1;
     }
-    if (stat(argv[1], &st) != 0) {
+    if (stat(argv[1], &st) != 0) {//通过路径获取bootloader文件属性, argv[1]为文件路径
         fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
         return -1;
     }
     printf("'%s' size: %lld bytes\n", argv[1], (long long)st.st_size);
-    if (st.st_size > 510) {
+    if (st.st_size > 510) {//结束标志是两个字节(0x55AA)
         fprintf(stderr, "%lld >> 510!!\n", (long long)st.st_size);
         return -1;
     }
     char buf[512];
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, sizeof(buf));//缓冲区清空
     FILE *ifp = fopen(argv[1], "rb");
     int size = fread(buf, 1, st.st_size, ifp);
     if (size != st.st_size) {
@@ -28,7 +27,7 @@ main(int argc, char *argv[]) {
         return -1;
     }
     fclose(ifp);
-    buf[510] = 0x55;
+    buf[510] = 0x55;//标记
     buf[511] = 0xAA;
     FILE *ofp = fopen(argv[2], "wb+");
     size = fwrite(buf, 1, 512, ofp);
